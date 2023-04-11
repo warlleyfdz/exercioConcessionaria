4e8b4044a61375816f5d960b816923febecf1f59
warlleyfdz <warlleyfdz@hotmail.com>
2023-04-11 10:21:09 -0300

    primeiro commit

---
+++ src/main/java/primeirotrampodev/exercicioConcessionaria/ExercicioConcessionariaApplication.java
@@ -0,0 +1,13 @@
+package primeirotrampodev.exercicioConcessionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioConcessionariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioConcessionariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/primeirotrampodev/exercicioConcessionaria/ExercicioConcessionariaApplicationTests.java
@@ -0,0 +1,13 @@
+package primeirotrampodev.exercicioConcessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioConcessionariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
